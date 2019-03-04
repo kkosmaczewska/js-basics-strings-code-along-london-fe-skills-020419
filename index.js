413ac2f8439e21aaac79f1778ba32abbd910f23b
Kasia Kosmaczewska <kakosmaczewska@gmail.com>
2019-03-04 20:32:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ const currentUser = 'Grace Hopper';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = '';
+
+const shortGreeting = 'Welcome, ';
